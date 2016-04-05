19aab54760710990a529886284ae9b3a22c5514e
Goran Ojkić <goran.ojkic@fyber.com>
2016-04-05 14:27:15 +0200

    Mark tests pendingUntilFixed

--- src/test/scala/com/example/MyServiceSpec.scala
+++ src/test/scala/com/example/MyServiceSpec.scala
@@ -3,30 +3,22 @@ package com.example
 import org.specs2.mutable.Specification
 import spray.testkit.Specs2RouteTest
 import spray.http._
-import StatusCodes._
 
 class MyServiceSpec extends Specification with Specs2RouteTest with MyService {
   def actorRefFactory = system
   
   "MyService" should {
 
-    "return a greeting for GET requests to the root path" in {
-      Get() ~> myRoute ~> check {
+    "return a proper bid response for bid_request" in {
+      Get("/bid_request") ~> myRoute ~> check {
         responseAs[String] must contain("Say hello")
       }
-    }
+    }.pendingUntilFixed
 
-    "leave GET requests to other paths unhandled" in {
-      Get("/kermit") ~> myRoute ~> check {
-        handled must beFalse
+    "return a proper winner response for winner request" in {
+      Get("/winner/6c831376-c1df-43ef-a377-85d83aa3314c") ~> myRoute ~> check {
+        responseAs[String] must contain("Say hello")
       }
-    }
-
-    "return a MethodNotAllowed error for PUT requests to the root path" in {
-      Put() ~> sealRoute(myRoute) ~> check {
-        status === MethodNotAllowed
-        responseAs[String] === "HTTP method not allowed, supported methods: GET"
-      }
-    }
+    }.pendingUntilFixed
   }
 }
